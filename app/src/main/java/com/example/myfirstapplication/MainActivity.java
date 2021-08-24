eef37c3ab37d0eb95d4b8bceda771f5200377501
Song Vu Nguyen <vunnems08@gmail.com>
2021-08-24 11:46:09 -0400

    Make text change when button clicked

--- app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void disable(View v){
         v.setEnabled(false);
+        Button button = (Button) v;
+        button.setText("Disabled");
         Log.d("success", "button disabled");
     }
 }
