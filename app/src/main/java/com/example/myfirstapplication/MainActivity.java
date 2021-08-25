77db6c1dbb1fee32769b9196c15e04ba54c3566e
Song Vu Nguyen <vunnems08@gmail.com>
2021-08-25 11:44:22 -0400

    Take input from users

--- app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,14 +18,21 @@ public class MainActivity extends AppCompatActivity {
         Log.d("fail", "it's not working");
     }
 
-    public void disable(View v){
-//        v.setEnabled(false);
-//        Button button = (Button) v;
-//        button.setText("Disabled");
-//        Log.d("success", "button disabled");
-        View myView = findViewById(R.id.button5);
-        myView.setEnabled(false);
-        Button button = (Button) myView;
-        button.setText("New Disabled");
+//    public void disable(View v){
+////        v.setEnabled(false);
+////        Button button = (Button) v;
+////        button.setText("Disabled");
+////        Log.d("success", "button disabled");
+//        View myView = findViewById(R.id.button5);
+//        myView.setEnabled(false);
+//        Button button = (Button) myView;
+//        button.setText("New Disabled");
+//    }
+
+    public void handleText(View v){
+        EditText t = findViewById(R.id.source);
+        String input = t.getText().toString();
+
+        Log.d("info", input);
     }
 }
